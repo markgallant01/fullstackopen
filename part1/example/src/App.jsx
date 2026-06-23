f4befb920782d68fc7504e47866bfc695eb0f70e
markgallant01 <markgallant01@gmail.com>
2026-06-23 03:28:52 -0400

    continuing examples for part 1d

--- part1/example/src/App.jsx
+++ part1/example/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
+      <button onClick={handleLeftClick}>
         left
       </button>
-      <button onClick={() => setRight(right + 1)}>
+      <button onClick={handleRightClick}>
         right
       </button>
       {right}
+      <p>{allClicks.join(" ")}</p>
+      <p>total {total}</p>
     </div>
   )
 }
