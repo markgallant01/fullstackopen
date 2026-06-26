745537f1d495c0282e9429efc1150c79bae1f401
markgallant01 <markgallant01@gmail.com>
2026-06-26 04:31:12 -0400

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, filterNames }) => {
   return (
@@ -35,14 +36,18 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState(
-    [
-      { name: 'Arto Hellas', number: '867-5309' }
-    ]
-  )
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
