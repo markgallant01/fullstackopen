359114dc8f6cc4f17d371f5048f43112a73e8c2f
markgallant01 <markgallant01@gmail.com>
2026-06-30 04:00:21 -0400

    exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Search = () => {
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all"
+
+const Search = ({ handleSearch }) => {
   return (
     <p>
-      find countries <input />
+      find countries <input onChange={handleSearch} />
     </p>
   )
 }
 
-const searchCountries = (searchTerm) => {
-  const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/name"
-  axios
-    .get(`${baseUrl}/${searchTerm}`)
-    .then(results => results.data)
+const CountryDisplay = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital {country.capital[0]}</p>
+      <p>Area {country.area}</p>
+      <h2>Languages</h2>
+      <ul>
+      {Object.keys(country.languages).map(key => {
+        return <li key={key}>{country.languages[key]}</li> 
+      })}
+      </ul>
+      <img src={country.flags["png"]} />
+    </div>
+  )
 }
 
-const Results = () => {
-  let searchResults
-  searchCountries("finland").then(results => {
-    searchResults = results
-    console.log(searchResults)
-  })
+const Results = ({ searchResults }) => {
+  if (searchResults.length === 0) {
+    return null
+  }
+
+  if (searchResults.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+
+  if (searchResults.length === 1) {
+    return <CountryDisplay country={searchResults[0]} />
+  }
+
+  return (
+    <div>
+      {searchResults.map(country => {
+        return <p key={country.name.common}>{country.name.common}</p>
+      })}
+    </div>
+  )
 }
 
 const App = () => {
+  const [countryData, setCountryData] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      setCountryData(response.data)
+    })
+  }, [])
+
+  const handleSearch = (event) => {
+    // search country list for search term
+    setSearchResults(countryData.filter(country => {
+      return country.name.common.toLowerCase().includes(event.target.value)
+    }))
+  }
+
   return (
     <div>
-      <Search />
-      <Results />
+      <Search handleSearch={handleSearch} />
+      <Results searchResults={searchResults} />
     </div>
   )
 }
